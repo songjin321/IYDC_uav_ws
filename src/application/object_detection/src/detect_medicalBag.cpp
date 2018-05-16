393f267999522932e00b5eb36ba63432568db071
SongJin <songjinxs@163.com>
2018-05-16 23:00:18 +0800

    detect medical bag ok, but not best

--- src/application/object_detection/src/detect_medicalBag.cpp
+++ src/application/object_detection/src/detect_medicalBag.cpp
@@ -3,20 +3,22 @@
 #include <limits>
 #include <algorithm>
 
-class SubscribeAndPublish
+class SubToBoxs
 {
 public:
-    SubscribeAndPublish()
+    SubToBoxs()
     {
         //Topic you want to publish
-        ros::Publisher box_pub = n_.advertise<std_msgs::Float32MultiArray>("/object_boxs", 1);
-
+        box_pub = n_.advertise<std_msgs::Float32MultiArray>("/object_boxs", 1);
+        std::cerr << "BiGodSayOK" << std::endl;
         //Topic you want to subscribe
-        objects_sub = n_.subscribe("/objects", 1, &SubscribeAndPublish::objects_sub_callback, this);
+        objects_sub = n_.subscribe("/objects", 1, &SubToBoxs::objects_sub_callback, this);
     }
 
     void objects_sub_callback(const std_msgs::Float32MultiArray& msg)
     {
+        if (msg.data.empty())
+            return;
         std_msgs::Float32MultiArray box;
         // the id of the object
         box.data.push_back(msg.data[0]);
@@ -41,8 +43,12 @@ public:
         float x2 = h21 * oh + h31;
         float y2 = h22 * oh + h32;
         float x3 = h11 * ow + h21 * oh + h31;
-        float y3 = h11 * ow + h22 * oh + h32;
-
+        float y3 = h12 * ow + h22 * oh + h32;
+        bool is_rectangle = isRectangle(x0,y0,x1,y1,x2,y2,x3,y3);
+        if(!is_rectangle)
+            return;
+        ROS_INFO("x0 = %f, x1 = %f, x2 = %f, x3 = %f, y0 = %f, y1 = %f, y2 = %f, y3 = %f",
+                 x0, x1, x2, x3, y0, y1, y2, y3);
         // upper left corner and lower right corner
         float xmin = std::min(x0, std::min(x1, std::min(x2, x3)));
         float xmax = std::max(x0, std::max(x1, std::max(x2, x3)));
@@ -50,24 +56,50 @@ public:
         float ymax = std::max(y0, std::max(y1, std::max(y2, y3)));
 
         box.data.push_back(xmin);
-        box.data.push_back(xmax);
+        box.data.push_back(ymin);
         box.data.push_back(xmax-xmin);
         box.data.push_back(ymax-ymin);
         box_pub.publish(box);
+        ROS_INFO("x = %f, y = %f, width = %f, high = %f",
+                 xmin, ymin, xmax-xmin, ymax-ymin);
     }
 
 private:
     ros::NodeHandle n_;
     ros::Publisher box_pub;
     ros::Subscriber objects_sub;
+private:
+    bool isRectangle(double x1, double y1,
+                     double x2, double y2,
+                     double x3, double y3,
+                     double x4, double y4)
+    {
+        double cx,cy;
+        double dd1,dd2,dd3,dd4;
 
+        cx=(x1+x2+x3+x4)/4;
+        cy=(y1+y2+y3+y4)/4;
+
+        dd1=sqrt((cx-x1)*(cx-x1)+(cy-y1)*(cy-y1));
+        dd2=sqrt((cx-x2)*(cx-x2)+(cy-y2)*(cy-y2));
+        dd3=sqrt((cx-x3)*(cx-x3)+(cy-y3)*(cy-y3));
+        dd4=sqrt((cx-x4)*(cx-x4)+(cy-y4)*(cy-y4));
+        double mean = (dd1 + dd2 + dd3 + dd4)/4;
+        double var = ((dd1-mean)*(dd1-mean) + (dd2-mean)*(dd2-mean)
+                    +(dd3-mean)*(dd3-mean) + (dd4-mean)*(dd4-mean))/4;
+        std::cout<<"mean = " << mean << " var = " << var << std::endl;
+        if (25 * sqrt(var) > mean)
+            return false;
+        else
+            return true;
+    }
 };//End of class SubscribeAndPublish
 
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "detect_medicalBag");
-    SubscribeAndPublish SAPObject;
+    SubToBoxs subToBoxs;
     ros::spin();
 }
 
--- src/application/object_detection/test/medicalBag_test.cpp
+++ src/application/object_detection/test/medicalBag_test.cpp
@@ -4,31 +4,40 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <image_transport/image_transport.h>
-
+class SubObjectBox;
 static const std::string OPENCV_WINDOW = "Image window";
 
-class ImageConverter
+class SubImageAndDrawObjectBox
 {
+
     ros::NodeHandle nh_;
     image_transport::ImageTransport it_;
     image_transport::Subscriber image_sub_;
     image_transport::Publisher image_pub_;
 
 public:
-    ImageConverter()
+    SubImageAndDrawObjectBox()
             : it_(nh_), box(0,0,0,0)
     {
         // Subscrive to input video feed and publish output video feed
         image_sub_ = it_.subscribe("/camera/image_raw", 1,
-                                   &ImageConverter::imageCb, this);
+                                   &SubImageAndDrawObjectBox::imageCb, this);
         image_pub_ = it_.advertise("/image_converter/output_video", 1);
-
+        sub_ = nh_.subscribe("/object_boxs", 1, &SubImageAndDrawObjectBox::drawBoxCallback, this);
         cv::namedWindow(OPENCV_WINDOW);
     }
 
-    ~ImageConverter()
+    ~SubImageAndDrawObjectBox()
     {
         cv::destroyWindow(OPENCV_WINDOW);
+    }
+
+    void drawBoxCallback(const std_msgs::Float32MultiArray& input)
+    {
+        box = cv::Rect2f(input.data[1], input.data[2], input.data[3], input.data[4]);
+        cv::rectangle(image, box, CV_RGB(255,0,0));
+        cv::imshow(OPENCV_WINDOW, image);
+        cv::waitKey(3);
     }
 
     void imageCb(const sensor_msgs::ImageConstPtr& msg)
@@ -43,64 +52,25 @@ public:
             ROS_ERROR("cv_bridge exception: %s", e.what());
             return;
         }
-
-        // Draw an example circle on the video stream
-        if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-            cv::rectangle(cv_ptr->image, box, CV_RGB(255,0,0));
-
+        image = cv_ptr->image;
         // Update GUI Window
-        cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+        cv::imshow(OPENCV_WINDOW, image);
         cv::waitKey(3);
-
-        // Output modified video stream
-        image_pub_.publish(cv_ptr->toImageMsg());
-    }
-    void setBox(cv::Rect2f &box)
-    {
-        this->box = box;
     }
 private:
-    cv::Rect2f box;
-};
-
-class SubObjectBox
-{
-public:
-    SubObjectBox():box(0,0,0,0)
-    {
-        // Topic you want to subscribe
-        sub_ = n_.subscribe("/object_boxs", 1, &SubObjectBox::callback, this);
-    }
-
-    void callback(const std_msgs::Float32MultiArray& input)
-    {
-        box = cv::Rect2f(input.data[1], input.data[2], input.data[3], input.data[4]);
-    }
-
-    cv::Rect2f getBox()
-    {
-        return box;
-    }
-private:
-    ros::NodeHandle n_;
     ros::Subscriber sub_;
     cv::Rect2f box;
-};//End of class SubObjectBox
+    cv::Mat image;
+};
 
 int main(int argc, char **argv)
 {
     //Initiate ROS
-    ros::init(argc, argv, "subscribe_and_publish");
+    ros::init(argc, argv, "medicalBag_test");
 
     // Subscribe objec
-    SubObjectBox sub_object_box;
-    ImageConverter ic;
+    SubImageAndDrawObjectBox o;
+    ros::spin();
 
-    while(ros::ok())
-    {
-        cv::Rect2f box = sub_object_box.getBox();
-        ic.setBox(box);
-        ros::spinOnce();
-    }
     return 0;
 }
